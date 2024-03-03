01a2af8ec71d2adbfa9c5e0d7b9155d1107d2278
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2024-03-03 02:31:30 -0300

    feat(repositories): create in memory gyms repository

---
+++ src/repositores/in-memory/in-memory-gyms.repository.ts
@@ -0,0 +1,34 @@
+import { Gym, Prisma } from "@prisma/client";
+import { Decimal } from "@prisma/client/runtime/library";
+import { randomUUID } from "node:crypto";
+
+import { GymsRepository } from "../@types/gyms-repository";
+
+export class InMemoryGymsRepository implements GymsRepository {
+  public items: Gym[] = [];
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym: Gym = {
+      latitude: new Decimal(data.latitude.toString()),
+      longitude: new Decimal(data.longitude.toString()),
+      title: data.title,
+      description: data.description ?? null,
+      id: data.id ?? randomUUID(),
+      phone: data.phone ?? null,
+    };
+
+    this.items.push(gym);
+
+    return gym;
+  }
+
+  async findById(gymId: string) {
+    const gym = this.items.find((item) => item.id === gymId);
+
+    if (!gym) {
+      return null;
+    }
+
+    return gym;
+  }
+}
