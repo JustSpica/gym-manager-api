5ef8332f7600aaf2b924839ca07a10a71bd59cbb
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2024-03-03 02:32:53 -0300

    feat(use-cases): create check ins use case

---
+++ src/use-cases/check-in.ts
@@ -0,0 +1,71 @@
+/* eslint-disable no-useless-constructor */
+import { CheckIn } from "@prisma/client";
+
+import { CheckInsRepository } from "@/repositores/@types/check-ins-repository";
+import { GymsRepository } from "@/repositores/@types/gyms-repository";
+import { getDistanceBetweenCoordenates } from "@/utils/get-distance-between-coordenates";
+
+import { MaxDistanceError } from "./errors/max-ditance.error";
+import { MaxNumberOfCheckInsError } from "./errors/max-number-off-check-ins.error";
+import { ResourceNotFoundError } from "./errors/resource-not-found.error";
+
+interface CheckInUseCaseRequest {
+  userId: string;
+  gymId: string;
+  userLatitude: number;
+  userLongitude: number;
+}
+
+interface CheckInUseCaseResponse {
+  checkIn: CheckIn;
+}
+
+export class CheckInUseCase {
+  constructor(
+    private checkInsRepository: CheckInsRepository,
+    private gymsRepository: GymsRepository,
+  ) {}
+
+  async execute({
+    gymId,
+    userId,
+    userLatitude,
+    userLongitude,
+  }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const gym = await this.gymsRepository.findById(gymId);
+
+    if (!gym) {
+      throw new ResourceNotFoundError();
+    }
+
+    const distance = getDistanceBetweenCoordenates(
+      { latitude: userLatitude, longitude: userLongitude },
+      {
+        latitude: gym.latitude.toNumber(),
+        longitude: gym.longitude.toNumber(),
+      },
+    );
+
+    const MAX_DISTANCE = 0.1;
+
+    if (distance > MAX_DISTANCE) {
+      throw new MaxDistanceError();
+    }
+
+    const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
+      userId,
+      new Date(),
+    );
+
+    if (checkInOnSameDay) {
+      throw new MaxNumberOfCheckInsError();
+    }
+
+    const checkIn = await this.checkInsRepository.create({
+      gym_id: gymId,
+      user_id: userId,
+    });
+
+    return { checkIn };
+  }
+}
